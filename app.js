e6a201ae7573147ab42a842af99d768cb74cf912
itedumail20 <itedumail20@gmail.com>
2024-05-31 08:31:49 -0700

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let modal = document.querySelector(".modal")
+function popUp(){
+    modal.style.display = "block"
+
+}
+
+
+function closeUp(){
+    // let modal = document.querySelector(".modal")
+    modal.style.display = "none"
+
+}
